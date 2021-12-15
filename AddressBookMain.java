d0f26c28cd3efeaf01b412b51363eac7503a6acf
rksgr <r_kjha@yahoo.com>
2021-12-15 11:41:28 +0530

    Refactored: create contacts in address book

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,10 +1,40 @@
 
 package com.mycompany.dec_14_addressbookmgmt;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 
 
 public class AddressBookMain {
-    public static void main(String []args){
-        System.out.println("Welcome to Address Book Program!");
+    /*
+    Use Case 1: Create contacts in address book
+    */
+    public void createContacts(){
+        // Initialize variables
+        String first_name = "first_name";
+        String last_name = "last_name";
+        String address = "address";
+        String city = "city";
+        String state = "state";
+        String zip = "zip";
+        String phone_num = "phone_num";
+        String email = "email";
+        
+        // Create a new arraylist of arrays to store details of all the persons
+        ArrayList<String[]> address_book = new ArrayList<String []>();
+        
+        // Array of string type to store contact details of a person
+        String contact[] = {first_name,last_name,address,city,state,zip,phone_num,email};
+        
+        // Add contact details of a person to address book(arraylist)
+        address_book.add(contact);
     }
     
+    
+    public static void main(String []args){
+        System.out.println("Welcome to Address Book Program!");
+        AddressBookMain abm  = new AddressBookMain();
+        abm.createContacts();
+        
+    }
 }
