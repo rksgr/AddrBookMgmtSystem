ace455a9b7ee7794262d0be92b6434bd02507782
rksgr <r_kjha@yahoo.com>
2021-12-15 12:16:24 +0530

    Refactored: Create contact in address book

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -9,32 +9,31 @@ public class AddressBookMain {
     /*
     Use Case 1: Create contacts in address book
     */
-    public void createContacts(){
-        // Initialize variables
-        String first_name = "first_name";
-        String last_name = "last_name";
-        String address = "address";
-        String city = "city";
-        String state = "state";
-        String zip = "zip";
-        String phone_num = "phone_num";
-        String email = "email";
+    public void createContacts(String first_name,String last_name,String address,
+            String city, String state, String zip, String phone_num,String email){
         
-        // Create a new arraylist of arrays to store details of all the persons
-        ArrayList<String[]> address_book = new ArrayList<String []>();
+        // Create a new arraylist of arraylist to store details of all the persons
+        ArrayList<ArrayList <String>> address_book = new ArrayList<ArrayList <String>>();
         
-        // Array of string type to store contact details of a person
-        String contact[] = {first_name,last_name,address,city,state,zip,phone_num,email};
-        
-        // Add contact details of a person to address book(arraylist)
-        address_book.add(contact);
+        // ArrayList of string type to store contact details of a person
+        ArrayList <String> contacts = new ArrayList <String>();
+        contacts.add(first_name);
+        contacts.add(last_name);
+        contacts.add(address);
+        contacts.add(city);        
+        contacts.add(state);        
+        contacts.add(zip);
+        contacts.add(phone_num);        
+        contacts.add(email);                 
+        // Add contact details of person to address book
+        address_book.add(contacts);
     }
     
     
     public static void main(String []args){
         System.out.println("Welcome to Address Book Program!");
         AddressBookMain abm  = new AddressBookMain();
-        abm.createContacts();
+        //abm.createContacts();
         
     }
 }
